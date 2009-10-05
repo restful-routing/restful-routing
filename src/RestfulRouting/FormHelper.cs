2c073949182f816e5debd8cc367edafc0b669081
stevehodgkiss <steve@hodgkiss.me.uk>
2009-10-05 20:36:54 +0100

    updated readme. refactored FormHelper

--- src/RestfulRouting/FormHelper.cs
+++ src/RestfulRouting/FormHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RestfulRouting
@@ -16,16 +15,14 @@ namespace RestfulRouting
 		public string DeleteFormFor(object entity, params object[] associatedEntities)
 		{
 			var id = IdFor(entity);
-			var form = "";
-			var dictionary = new RouteValueDictionary { };
-			
-			AddAssociatedEntitiesToDictionary(associatedEntities, dictionary);
+
+			var dictionary = PopulateDictionaryWithAssociatedEntityIds(associatedEntities);
 
 			dictionary.Add("id", id);
 
 			var url = _url.Action("destroy", LowerCasePluralName(entity), dictionary);
 
-			form = string.Format("<form method=\"post\" action=\"{0}\">{1}", url, OverrideTag("delete"));
+			var form = FormTag(url) + OverrideTag("delete");
 
 			return form;
 		}
@@ -33,52 +30,62 @@ namespace RestfulRouting
 		public string FormFor(object entity, params object[] associatedEntities)
 		{
 			var id = IdFor(entity);
-			var form = "";
-			var dictionary = new RouteValueDictionary {};
-			AddAssociatedEntitiesToDictionary(associatedEntities, dictionary);
+			
+			string form;
+
+			var dictionary = PopulateDictionaryWithAssociatedEntityIds(associatedEntities);
 
 			if (string.IsNullOrEmpty(id) || id == "0" || id == "-1")
 			{
 				var url = _url.Action("create", LowerCasePluralName(entity), dictionary);
-				form = string.Format("<form method=\"post\" action=\"{0}\">", url);
+
+				form = FormTag(url);
 			}
 			else
 			{
 				dictionary.Add("id", id);
+
 				var url = _url.Action("update", LowerCasePluralName(entity), dictionary);
-				form = string.Format("<form method=\"post\" action=\"{0}\">{1}", url, OverrideTag("put"));
+
+				form = FormTag(url) + OverrideTag("put");
 			}
 
 			return form;
 		}
 
-		private void AddAssociatedEntitiesToDictionary(object[] entities, RouteValueDictionary dictionary)
+		private static RouteValueDictionary PopulateDictionaryWithAssociatedEntityIds(object[] entities)
 		{
+			var dictionary = new RouteValueDictionary();
 			foreach (var associatedEntity in entities)
 			{
 				dictionary.Add(associatedEntity.GetType().Name.ToLowerInvariant() + "Id", IdFor(associatedEntity));
 			}
+			return dictionary;
 		}
 
-		private string OverrideTag(string method)
+		private static string FormTag(string url)
+		{
+			return string.Format("<form method=\"post\" action=\"{0}\">", url);
+		}
+
+		private static string OverrideTag(string method)
 		{
 			return string.Format("<input type=\"hidden\" name=\"_method\" value=\"{0}\" />", method);
 		}
 
-		private string LowerCasePluralName(object entity)
+		private static string LowerCasePluralName(object entity)
 		{
 			var name = entity.GetType().Name.ToLowerInvariant();
 			return Inflector.Net.Inflector.Pluralize(name);
 		}
 
-		private string IdFor(object entity)
+		private static string IdFor(object entity)
 		{
 			// TODO make this configurable
 			var idProperty = entity.GetType().GetProperty("Id");
 
 			var id = idProperty.GetValue(entity, null);
-			if (id == null)
-				return null;
+
 			return id.ToString();
 		}
 	}
