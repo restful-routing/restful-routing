ce74474cd73819532838fa15e8a49a0dff2296e2
unknown <Administrator@.(none)>
2010-03-21 10:24:19 +0000

    added custom route mapping

---
+++ src/RestfulRouting.Tests/Unit/Mappings/RouteSpec.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Machine.Specifications;
+using RestfulRouting.Mappings;
+
+namespace RestfulRouting.Tests.Unit.Mappings
+{
+	[Subject(typeof(RouteMapping))]
+	public class when_mapping_custom_route_mapping
+	{
+		static RouteMapping mapping;
+
+		static RouteCollection collection;
+
+		static Route route;
+
+		private Establish context = () =>
+		                            	{
+											collection = new RouteCollection();
+		                            		route = new Route("test", new MvcRouteHandler());
+		                            		mapping = new RouteMapping(route);
+		                            	};
+
+		Because of = () => mapping.AddRoutesTo(collection);
+
+		It should_add_the_given_route_to_the_collection = () => mapping.Route.ShouldEqual(route);
+	}
+}
---
+++ src/RestfulRouting/Mappings/RouteMapping.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Routing;
+
+namespace RestfulRouting.Mappings
+{
+	public class RouteMapping : Mapping
+	{
+		public RouteBase Route;
+
+		public RouteMapping(RouteBase route)
+		{
+			Route = route;
+		}
+
+		public override void AddRoutesTo(RouteCollection routeCollection)
+		{
+			routeCollection.Add(Route);
+		}
+	}
+}
