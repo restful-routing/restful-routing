a2c5fbc76d14fc940c3f2fd6c10cc82c5dffadf5
Steve Hodgkiss <steve@hodgkiss.me.uk>
2010-04-08 22:17:24 +0100

    Add VS2010 solution. Refactor sample routes.

--- src/RestfulRouting.Sample/Global.asax.cs
+++ src/RestfulRouting.Sample/Global.asax.cs
@@ -21,41 +21,4 @@ namespace RestfulRouting.Sample
 			RegisterRoutes(RouteTable.Routes);
 		}
 	}
-
-	public class Routes : RouteSet
-	{
-		public Routes()
-		{
-			Map("").To<RootController>(x => x.Index());
-
-			Map("routedebug").To<RouteDebugController>(x => x.Index());
-
-			//Area<BlogsController>("", () =>
-			//{
-			//    Resources<BlogsController>(() =>
-			//    {
-			//        As("weblogs");
-			//        Only("index", "show");
-			//        Collection("latest", HttpVerbs.Get);
-
-			//        Resources<PostsController>(() =>
-			//        {
-			//            Except("create", "update", "destroy");
-			//            Resources<CommentsController>(() => Except("destroy"));
-			//        });
-			//    });
-
-			//});
-
-			Area<BlogsController>("", () =>
-											{
-												Resources<BlogsController>(() => Resources<PostsController>());
-											});
-			Area<Controllers.Admin.BlogsController>("admin", () =>
-																{
-																	Resources<Controllers.Admin.BlogsController>();
-																	Resources<Controllers.Admin.PostsController>();
-																});
-		}
-	}
 }
---
+++ src/RestfulRouting.Sample/Routes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestfulRouting.Sample.Controllers;
+
+namespace RestfulRouting.Sample
+{
+    public class Routes : RouteSet
+    {
+        public Routes()
+        {
+            Map("").To<RootController>(x => x.Index());
+
+            Map("routedebug").To<RouteDebugController>(x => x.Index());
+
+            Area<BlogsController>("", () =>
+            {
+                Resources<BlogsController>(() => {
+                    Resources<PostsController>();
+                });                
+            });
+
+
+            Area<Controllers.Admin.BlogsController>("admin", () =>
+            {
+                Resources<Controllers.Admin.BlogsController>();
+                Resources<Controllers.Admin.PostsController>();
+            });
+        }
+    }
+}
