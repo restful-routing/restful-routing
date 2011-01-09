efc6f6cf97c6fad05c5da3a98100e2bf83fbbb05
Troels Thomsen <tt@codesensus.com>
2011-01-09 12:12:52 -0800

    Add new method stub for generating routes

--- src/RestfulRouting/Mapper.cs
+++ src/RestfulRouting/Mapper.cs
@@ -1,6 +1,13 @@
-﻿namespace RestfulRouting
+﻿using System;
+using System.Web.Routing;
+
+namespace RestfulRouting
 {
 	public abstract class Mapper
 	{
+		protected Route GenerateRoute(string path, string controller, string action, string[] httpMethods)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
