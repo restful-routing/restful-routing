44983291e178c7952e7bd14715af78c793715e2c
Troels Thomsen <tt@codesensus.com>
2011-01-09 12:15:28 -0800

    Implement method

--- src/RestfulRouting/Mapper.cs
+++ src/RestfulRouting/Mapper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RestfulRouting
@@ -7,7 +7,10 @@ namespace RestfulRouting
 	{
 		protected Route GenerateRoute(string path, string controller, string action, string[] httpMethods)
 		{
-			throw new NotImplementedException();
+			return new Route(path,
+				new RouteValueDictionary(new { controller, action }),
+				new RouteValueDictionary(new { httpMethod = new HttpMethodConstraint(httpMethods) }),
+				new MvcRouteHandler());
 		}
 	}
 }
