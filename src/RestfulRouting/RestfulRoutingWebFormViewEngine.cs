604570ed9393f7453cc80a2c7578fd21e1ad5685
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2011-02-16 11:55:20 -0500

    added Razor and Webform view engines, obsoleted original RestfulRoutingViewEngine.

---
+++ src/RestfulRouting/RestfulRoutingWebFormViewEngine.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RestfulRouting
+{
+    public class RestfulRoutingWebFormViewEngine : WebFormViewEngine
+    {
+        public RestfulRoutingWebFormViewEngine()
+        {
+            AreaMasterLocationFormats = new[] {
+                                                  "~/Views/{2}/{1}/{0}.master",
+                                                  "~/Views/{2}/Shared/{0}.master",
+                                              };
+
+            AreaViewLocationFormats = new[] {
+                                                "~/Views/{2}/{1}/{0}.aspx",
+                                                "~/Views/{2}/{1}/{0}.ascx",
+                                                "~/Views/{2}/Shared/{0}.aspx",
+                                                "~/Views/{2}/Shared/{0}.ascx",
+                                            };
+
+            AreaPartialViewLocationFormats = AreaViewLocationFormats;
+        }
+    }
+}
