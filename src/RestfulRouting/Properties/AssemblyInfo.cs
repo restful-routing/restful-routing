549db22807c1251ddbd33ab2c523bf0fb50a55d6
Steve Hodgkiss <steve@hodgkiss.me.uk>
2011-03-22 13:39:45 +0000

    Update assemblyinfo

--- src/RestfulRouting/Properties/AssemblyInfo.cs
+++ src/RestfulRouting/Properties/AssemblyInfo.cs
@@ -1,36 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RestfulRouting is a routing library for ASP.NET MVC based on the Rails 3 routing DSL.")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("RestfulRouting")]
+[assembly: AssemblyDescription("RestfulRouting is a routing library for ASP.NET MVC based on the Rails 3 routing DSL.")]
 [assembly: AssemblyProduct("RestfulRouting")]
 [assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d58bb36a-e28c-49a3-91ca-8516782ed441")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
