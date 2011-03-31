9a1b7be6ef489a70981f0e2230e37cb7d2cf93e9
Steve Hodgkiss <steve@hodgkiss.me.uk>
2011-03-31 17:33:07 +0100

    Move format collection to a new file

---
+++ src/RestfulRouting/Format/FormatCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace RestfulRouting.Format
+{
+    public class FormatCollection : Dictionary<string, Func<ActionResult>>
+    {
+        public ActionResult Default { get; set; }
+
+        public Func<ActionResult> Html { set { this["html"] = value; } }
+
+        public Func<ActionResult> Xml { set { this["xml"] = value; } }
+
+        public Func<ActionResult> Json { set { this["json"] = value; } }
+
+        public Func<ActionResult> Js { set { this["js"] = value; } }
+
+        public Func<ActionResult> Csv { set { this["csv"] = value; } }
+    }
+}
