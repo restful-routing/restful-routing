b73a6db9f334ba2f1f7aa35c4971ee5b040b3920
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2011-10-02 17:55:32 -0400

    moving to version 1.1.3

--- src/RestfulRouting/Properties/AssemblyInfo.cs
+++ src/RestfulRouting/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
 [assembly: AssemblyTitle("RestfulRouting")]
 [assembly: AssemblyDescription("RestfulRouting is a routing library for ASP.NET MVC based on the Rails 3 routing DSL.")]
 [assembly: AssemblyProduct("RestfulRouting")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.3")]
 
