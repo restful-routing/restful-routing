f967a60df27df72d81ba1b85067fd980a744edd5
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2012-03-27 07:40:06 -0400

    removed dependency on Razor View engine, will help with migration to MVC 4.

--- src/RestfulRouting/RouteDebug/RouteDebugController.cs
+++ src/RestfulRouting/RouteDebug/RouteDebugController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using System.Web.Routing;
-using RazorEngine;
 
 namespace RestfulRouting.RouteDebug
 {
@@ -81,10 +80,33 @@ namespace RestfulRouting.RouteDebug
                              where p.Endpoint.Equals("routedebug#resources", StringComparison.InvariantCultureIgnoreCase)
                              select p.Path.Replace("{name}", string.Empty)).FirstOrDefault();
             model.DebugPath = debugPath;
-
-            var template = GetTemplate();
-            return Content(Razor.Parse(template, model));
+     
+            return Content(Debugger(model));
         }
+
+        public string Debugger(RouteDebugViewModel model) 
+        {
+            var style = model.GetPath("style.css");
+            var jqueryDatatables = model.GetPath("jquery.dataTables.min.js");
+
+            var routeInfos = new StringBuilder();
+
+            foreach (var routeinfo in model.RouteInfos) 
+            {
+                routeInfos.AppendLine("<tr>");
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.Position);
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.Name);
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.HttpMethod);
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.Area);
+                routeInfos.AppendFormat("<td class='path'><a href='{0}' target='_blank'>{0}</a></td>", routeinfo.Path);
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.Endpoint);
+                routeInfos.AppendFormat("<td>{0}</td>", routeinfo.Namespaces);
+                routeInfos.AppendLine("</tr>");
+            }
+
+            return string.Format(HtmlPage, style, jqueryDatatables, routeInfos);
+        }
+
         public ActionResult Resources(string name)
         {
             try
@@ -127,25 +149,75 @@ namespace RestfulRouting.RouteDebug
                 return HttpNotFound();
             }
         }
-
-        private string GetTemplate()
+     
+        private string GetContent(string name)
         {
-            using (var reader = new StreamReader(GetType().Assembly.GetManifestResourceStream("RestfulRouting.RouteDebug.Content.Index.cshtml"))) {
-                return reader.ReadToEnd();
-            }
-        }
-
-        private string GetContent(string name) {
-            try {
+            try 
+            {
                 using (var reader = new StreamReader(GetType().Assembly.GetManifestResourceStream(string.Format("RestfulRouting.RouteDebug.Content.{0}", name))))
                 {
                     return reader.ReadToEnd();
                 }
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 return string.Empty;
             }
         }
+
+        const string HtmlPage =
+@"<!DOCTYPE HTML>
+<html lang='en-GB'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Restful Routing</title>
+    <link href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/themes/hot-sneaks/jquery-ui.css' rel='stylesheet' type='text/css' />
+    <link href='{0}' rel='stylesheet' type='text/css' />
+    <script src='http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5.1.min.js' type='text/javascript'></script>
+    <script src='{1}' type='text/javascript'></script>
+    <script type='text/javascript'>
+        $(document).ready(function () {{
+            $('#routes').dataTable({{
+                'bJQueryUI': true,
+                'sPaginationType': 'full_numbers',
+                'bLengthChange': false,
+                'bAutoWidth': true,
+                'bPaginate':false
+            }});
+        }});
+    </script>
+</head>
+<body>
+  <div id='container'>
+    <div id='header'>
+        <h1>restful-routing</h1>
+        <h2>route debugger</h2>
+        <p>
+            <a href='http://stevehodgkiss.github.com/restful-routing/'>For more help go to the wiki page at http://stevehodgkiss.github.com/restful-routing/ </a>
+        </p>
+    </div>
+    <div id='routes-container'>
+        <table id='routes'>
+          <thead>
+            <tr>
+                <th>Position</th>
+                <th>Name</th>
+                <th>HttpMethods</th>
+                <th>Area</th>
+                <th>Path</th>
+                <th>Endpoint</th>
+                <th>Namespaces</th>
+            </tr>
+          </thead>
+          <tbody>
+            {2}
+          </tbody>
+        </table>
+    </div>
+  </div>
+  <a href='http://github.com/stevehodgkiss/restful-routing' id='github'>
+      <img src='http://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'>
+    </a>
+</body>
+</html>";
     }
 }
