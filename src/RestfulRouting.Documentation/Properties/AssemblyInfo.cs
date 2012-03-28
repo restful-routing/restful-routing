bc5f1e8123f76582e7c95f83492d5a7e3723e32b
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2012-03-28 16:48:39 -0400

    added documentation web application.

---
+++ src/RestfulRouting.Documentation/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestfulRouting.Documentation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RestfulRouting.Documentation")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a28ed6c-eb60-43c5-a015-6c20d11a5fe8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
