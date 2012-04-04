849b49551ad7384686e5e40b504326520f9dbb37
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2012-04-03 20:50:16 -0400

    added sub nav.

--- src/RestfulRouting.Documentation/Scripts/restful.routing.js
+++ src/RestfulRouting.Documentation/Scripts/restful.routing.js
@@ -1,3 +1,9 @@
 ﻿$(function () {
     window.prettyPrint() && prettyPrint();
+
+    // Disable certain links in docs
+    $('section [href^=#]').click(function(e) {
+        e.preventDefault();
+    });
 });
+
