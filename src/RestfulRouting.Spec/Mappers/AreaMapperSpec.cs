fdab72a108d35abbdd36baa2cb89af76e3e49dae
Adrian Phinney <adrian.phinney@bellaliant.net>
2013-07-16 13:33:20 -0300

    Add missing pathPrefix parameter for AreaMapper.

--- src/RestfulRouting.Spec/Mappers/AreaMapperSpec.cs
+++ src/RestfulRouting.Spec/Mappers/AreaMapperSpec.cs
@@ -4,12 +4,14 @@ using Machine.Specifications;
 using RestfulRouting.Mappers;
 using RestfulRouting.Spec.TestObjects;
 using System.Linq;
+using System.Web.Mvc;
+using MvcContrib.TestHelper;
 
 namespace RestfulRouting.Spec.Mappers
 {
     public class area_mapper : base_context
     {
-        static AreaMapper areaMapper = new AreaMapper("test", typeof(PostsController).Namespace, x => x.Resources<PostsController>());
+        static AreaMapper areaMapper = new AreaMapper("test", null, typeof(PostsController).Namespace, x => x.Resources<PostsController>());
 
         Because of = () => areaMapper.RegisterRoutes(routes);
 
@@ -22,5 +24,17 @@ namespace RestfulRouting.Spec.Mappers
         It sets_namespace_fallback = () => routes.ShouldEachConformTo(x => (bool)DataTokens(x)["UseNamespaceFallback"] == false);
         
         It adds_to_the_resource_path = () => areaMapper.JoinResources("posts").ShouldEqual("test_posts");
+    }
+
+    public class area_mapper_with_path_prefix : base_context
+    {
+        static AreaMapper areaMapper = new AreaMapper("admin", "top/secret", null, map => map.Root<PostsController>(x => x.Index()));
+        Because of = () => areaMapper.RegisterRoutes(routes);
+
+        It uses_the_path_prefix_for_the_path = () => "~/top/secret".WithMethod(HttpVerbs.Get).ShouldMapTo<PostsController>(x => x.Index());
+
+        It sets_the_area = () => routes.ShouldEachConformTo(x => ((Route)x).DataTokens["area"].ToString() == "admin");
+
+        It adds_to_the_resource_path = () => areaMapper.JoinResources("posts").ShouldEqual("top/secret_posts");
     }
 }
