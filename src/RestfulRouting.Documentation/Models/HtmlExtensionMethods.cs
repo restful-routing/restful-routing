f517649e9ee77838f01a90b7c7cda63669965a15
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2013-12-20 11:43:32 -0500

    a few changes so it can deploy to azure.

---
+++ src/RestfulRouting.Documentation/Models/HtmlExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestfulRouting.Documentation.Models
+{
+    public static class HtmlExtensionMethods
+    {
+        public static string GetVersion(this HtmlHelper helper)
+        {
+            var version = typeof (RouteSet).Assembly.GetName().Version;
+            return string.Format("({0})", version);
+        }
+    }
+}
